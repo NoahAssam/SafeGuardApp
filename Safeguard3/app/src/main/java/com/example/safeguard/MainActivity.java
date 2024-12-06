5da68ac89ad67c60d89b3ccc9cabce995ae4eee8
Samarth-1012 <106692036+Samarth-1012@users.noreply.github.com>
2024-12-05 19:22:01 -0500

    added, MainActivity.java, CHANGED THE SIGN IN FILE

--- Safeguard3/app/src/main/java/com/example/safeguard/MainActivity.java
+++ Safeguard3/app/src/main/java/com/example/safeguard/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.safeguard;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView alarmIndicator;
+    private boolean isAlarmOn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);  // Make sure you have 'activity_main.xml'
+        setContentView(R.layout.activity_main);
+
+        // Find the alarm indicator
+        alarmIndicator = findViewById(R.id.alarmIndicator);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the TILDE key (KEYCODE_GRAVE) is pressed
+        if (keyCode == KeyEvent.KEYCODE_GRAVE) {
+            toggleAlarm(); // Call the toggle method
+            return true; // Indicate that the key event has been handled
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void toggleAlarm() {
+        if (isAlarmOn) {
+            // Turn OFF the alarm
+            alarmIndicator.setText("ALARM: OFF");
+            alarmIndicator.setBackgroundColor(getResources().getColor(R.color.green));
+        } else {
+            // Turn ON the alarm
+            alarmIndicator.setText("ALARM: ON");
+            alarmIndicator.setBackgroundColor(getResources().getColor(R.color.red));
+        }
+        isAlarmOn = !isAlarmOn; // Toggle the state
     }
 }
